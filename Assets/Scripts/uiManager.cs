b183ef5bcf236e8dbca7a2123c4fa26c5e23d4c1
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-05-03 15:13:41 +0200

    added first draft of home scene

---
+++ Assets/Scripts/uiManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiManager : MonoBehaviour
+{
+    public void Launch()
+    {
+        SceneManager.LoadScene("Launch");
+    }
+    public void Rocket()
+    {
+        SceneManager.LoadScene("Launch");
+    }
+    public void Config()
+    {
+        SceneManager.LoadScene("Launch");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
