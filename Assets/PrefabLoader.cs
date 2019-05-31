d91d2830688f7cb51ba8ddc09279de18dbeb2ba3
Yannick Pulver <yannick.pulver@gmail.com>
2019-05-31 23:27:12 +0200

    Added assets + new scene

---
+++ Assets/PrefabLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabLoader : MonoBehaviour
+{
+    public GameObject Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void LoadPrefab()
+    {
+        print("PRes");
+        Instantiate(Prefab);
+        Destroy(gameObject);
+    }
+}
