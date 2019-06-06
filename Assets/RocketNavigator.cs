fea18e500e7ebdda7b2473dfedc3a79ba755f3de
Unknown <cedric.merz@students.fhnw.ch>
2019-06-06 14:11:35 +0200

    enable tilt function

--- Assets/RocketNavigator.cs
+++ Assets/RocketNavigator.cs
@@ -8,6 +8,9 @@ public class RocketNavigator : MonoBehaviour
     public Rigidbody2D rb;
     public GameObject flame;
 
+    float speedX;
+    public float speed = 2f;
+
     void Update()
     {
         flame.SetActive(rb.velocity.y > 0);
@@ -15,6 +18,12 @@ public class RocketNavigator : MonoBehaviour
 
     private void OnMouseDown()
     {
+        speedX = Input.acceleration.x * speed;
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -2f, 2f), transform.position.y);
+
+        rb.velocity = new Vector2(speedX, 0f);
+
         rb.AddForce(Vector3.up * 100);
     }
+
 }
