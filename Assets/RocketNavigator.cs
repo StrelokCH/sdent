487a3f2e4fa2c5d87aaa820ef2bfef028f0164e4
Yannick Pulver <yannick.pulver@gmail.com>
2019-06-06 13:21:47 +0200

    updated camera movement

---
+++ Assets/RocketNavigator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketNavigator : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject flame;
+
+    void Update()
+    {
+        flame.SetActive(rb.velocity.y > 0);
+    }
+
+    private void OnMouseDown()
+    {
+        rb.AddForce(Vector3.up * 100);
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject starPrefab;
+    public Transform starContainer;
+
+    public int numberOfStars = 500;
+
+    public float levelWidth = 2f;
+    public float minY = .01f;
+    public float maxY = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3(0,0,1);
+
+        for (int i = 0; i < numberOfStars; i++)
+        {
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            spawnPosition.y += Random.Range(minY, maxY);
+            var star = Instantiate(starPrefab, spawnPosition, Quaternion.identity, starContainer);
+
+            var scale = Random.Range(-0.2f, 0f);
+            
+            star.transform.localScale = new Vector3(scale,scale,scale);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
