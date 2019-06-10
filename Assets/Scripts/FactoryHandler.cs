3beef712a6d6b2311464784ca0ea22627c255869
Yannick Pulver <yannick.pulver@gmail.com>
2019-06-10 18:18:57 +0200

    updated fabrik

--- Assets/Scripts/FactoryHandler.cs
+++ Assets/Scripts/FactoryHandler.cs
@@ -5,14 +5,20 @@ public class FactoryHandler : MonoBehaviour
 {
     private int step = 1;
 
+    public Text Title;
+    public Text Description;
+
     public GameObject[] Bodies;
     public GameObject[] Flames;
     public GameObject[] Wings;
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         ShowSelected();
+        Change(0);
     }
 
     public void OnLeftClick()
@@ -28,12 +34,18 @@ public class FactoryHandler : MonoBehaviour
         {
             case 1:
                 GameHandler.Instance.Rocket.Body = (GameHandler.Instance.Rocket.Body + signum) % 3;
+                Title.text = FactoryTexts.BodyTitle[GameHandler.Instance.Rocket.Body];
+                Description.text = FactoryTexts.BodyDescription[GameHandler.Instance.Rocket.Body];
                 break;
             case 2:
                 GameHandler.Instance.Rocket.Wings = (GameHandler.Instance.Rocket.Wings + signum) % 3;
+                Title.text = FactoryTexts.WingsTitle[GameHandler.Instance.Rocket.Wings];
+                Description.text = FactoryTexts.WingsDescription[GameHandler.Instance.Rocket.Wings];
                 break;
             case 3:
                 GameHandler.Instance.Rocket.Flame = (GameHandler.Instance.Rocket.Flame + signum) % 3;
+                Title.text = FactoryTexts.FlameTitle[GameHandler.Instance.Rocket.Flame];
+                Description.text = FactoryTexts.FlameDescription[GameHandler.Instance.Rocket.Flame];
                 break;
         }
     }
@@ -51,6 +63,7 @@ public class FactoryHandler : MonoBehaviour
         {
             step++;
             GameObject.Find("Subtitle").GetComponent<Image>().sprite = Resources.Load<Sprite>("subtitle_" + step);
+            Change(0);
         }
         else
         {
---
+++ Assets/Scripts/FactoryTexts.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryTexts : MonoBehaviour
+{
+    public static string[] BodyTitle = {"The Egg", "The Bullet", "The Edgy"};
+    public static string[] BodyDescription = {"Easy to fly \nAll(round)er", "Strong, powerful\nHeavy", "Futuristic\nSlightly weird"};
+    
+    public static string[] WingsTitle = {"The Bat", "The Moth", "The Panel"};
+    public static string[] WingsDescription = {"As smooth as it gets", "Likes light", "No wings, just \nsolar panels"};
+    
+    public static string[] FlameTitle = {"Regular", "Eco-Friendly", "Elon's Mix"};
+    public static string[] FlameDescription = {"They flew to the \nmoon with this", "Save for this planet,\nmaybe for the next, too?", "Usually on high speed\nStops sometimes"};
+    
+    
+}
