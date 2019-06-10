fa4ca3eb0d73c1ea2b7c3923abfa3f5e94ff7a80
Yannick Pulver <yannick.pulver@gmail.com>
2019-06-10 19:27:38 +0200

    updated level logic

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -9,12 +9,12 @@ public class LevelGenerator : MonoBehaviour
     public Transform starContainer;
     public GameObject rocket;
 
-    public int numberOfStars = 500;
+    public int numberOfStars = 1000;
 
     public static float levelWidth = 2f;
     public static float levelHeight = 8f;
     public float minY = .01f;
-    public float maxY = .1f;
+    public float maxY = .05f;
 
     bool flying = false;
     readonly float flyingHeight = 20f;
@@ -24,21 +24,24 @@ public class LevelGenerator : MonoBehaviour
     {
         var cameraFollow = Camera.main.gameObject.AddComponent<CameraFollow>();
         cameraFollow.target = rocket.transform;
-        
+
         Vector3 spawnPosition = new Vector3(0, 0, 1);
 
         for (int i = 0; i < numberOfStars; i++)
         {
-            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
             spawnPosition.y += Random.Range(minY, maxY);
-            var star = Instantiate(starPrefab, spawnPosition, Quaternion.identity, starContainer);
-
-            var scale = Random.Range(-0.2f, 0f);
-
-            star.transform.localScale = new Vector3(scale, scale, scale);
+            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            SpawnStar(spawnPosition);
         }
 
         InvokeRepeating("CreateAsteroid", 5f, 1.5f);
+    }
+
+    private void SpawnStar(Vector3 spawnPosition)
+    {
+        var star = Instantiate(starPrefab, spawnPosition, Quaternion.identity, starContainer);
+        var scale = Random.Range(-0.2f, 0f);
+        star.transform.localScale = new Vector3(scale, scale, scale);
     }
 
     void CreateAsteroid()
