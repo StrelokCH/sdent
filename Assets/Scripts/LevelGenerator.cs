ba5e8aa47f60b125af4847a65278f95a9a8daa49
Yannick Pulver <yannick.pulver@gmail.com>
2019-06-10 21:20:19 +0200

    Updated Star Spawning: Generation after time

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -10,39 +10,56 @@ public class LevelGenerator : MonoBehaviour
     public Transform asteroidContainer;
     public GameObject rocket;
 
-    public int numberOfStars = 1000;
+    public int numberOfStars = 200;
 
     public static float levelWidth = 2f;
     public static float levelHeight = 8f;
     public float minY = .01f;
     public float maxY = .05f;
 
-    bool flying = false;
+    bool flying;
     readonly float flyingHeight = 20f;
+    Vector3 starSpawnPosition = new Vector3(0, 0, 1);
 
     // Start is called before the first frame update
     void Start()
     {
         var cameraFollow = Camera.main.gameObject.AddComponent<CameraFollow>();
         cameraFollow.target = rocket.transform;
-
-        Vector3 spawnPosition = new Vector3(0, 0, 1);
-
-        for (int i = 0; i < numberOfStars; i++)
-        {
-            spawnPosition.y += Random.Range(minY, maxY);
-            spawnPosition.x = Random.Range(-levelWidth, levelWidth);
-            SpawnStar(spawnPosition);
-        }
+        SpawnStars();
+        
 
         InvokeRepeating("CreateAsteroid", 5f, 1.5f);
     }
 
-    private void SpawnStar(Vector3 spawnPosition)
+    void Update()
     {
-        var star = Instantiate(starPrefab, spawnPosition, Quaternion.identity, starContainer);
-        var scale = Random.Range(-0.2f, 0f);
-        star.transform.localScale = new Vector3(scale, scale, scale);
+        if (!flying)
+        {
+            var position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.height / 2, Screen.width / 2, 0));
+            if (position.y > flyingHeight)
+            {
+                flying = true;
+            }
+        }
+        
+        if (rocket.transform.position.y > starSpawnPosition.y - 20)
+        {
+            SpawnStars();
+        }
+    }
+
+    private void SpawnStars()
+    {
+        for (int i = 0; i < numberOfStars; i++)
+        {
+            starSpawnPosition.y += Random.Range(minY, maxY);
+            starSpawnPosition.x = Random.Range(-levelWidth, levelWidth);
+            var star = Instantiate(starPrefab, starSpawnPosition, Quaternion.identity, starContainer);
+            var scale = Random.Range(-0.2f, 0f);
+            star.transform.localScale = new Vector3(scale, scale, scale);
+        }
+
     }
 
     void CreateAsteroid()
@@ -50,20 +67,6 @@ public class LevelGenerator : MonoBehaviour
         if (flying)
         {
             Instantiate(asteroidPrefab, asteroidContainer);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!flying)
-        {
-            var position =
-                Camera.main.ScreenToWorldPoint(new Vector3(Screen.height / 2, Screen.width / 2, 0));
-            if (position.y > flyingHeight)
-            {
-                flying = true;
-            }
         }
     }
 }
