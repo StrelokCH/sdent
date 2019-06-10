0f2807fc8435ab546b2929835d4dfe302386347d
Unknown <cedric.merz@students.fhnw.ch>
2019-06-10 17:25:07 +0200

    added comments

--- Assets/RocketNavigator.cs
+++ Assets/RocketNavigator.cs
@@ -11,13 +11,9 @@ public class RocketNavigator : MonoBehaviour
 
     private bool rotating = true;
 
-    private Vector3 inputX = Vector3.zero;
+    private Vector3 inputUp;
 
     public Text text;
-    void Start()
-    {
-
-    }
 
     void Update()
     {
@@ -30,41 +26,20 @@ public class RocketNavigator : MonoBehaviour
         flame.SetActive(Input.touchCount > 0);
         if (Input.touchCount > 0)
         {
+            // Max. Upwards Velocity
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, 9f);
 
-            inputX = Vector3.up * 15f;
-
+            // Add Forces
+            inputUp  = Vector3.up * 15;
             rb.AddRelativeForce((15 * Input.acceleration.x * Vector3.right));
-            rb.AddRelativeForce(inputX + transform.rotation.eulerAngles * 100);
+            rb.AddRelativeForce(inputUp + transform.rotation.eulerAngles * 100);
 
+            // Add Rotation to the rocket object
             transform.rotation = Quaternion.Euler(0, 0, (-Input.acceleration.x * 50) * 2);
 
+            // For testing the values of Input.acceleration
             text.text = Input.acceleration.x.ToString();
-
-            /*  if (rotating && Input.acceleration.x > 0)
-             {
-                 Vector3 to = new Vector3(0, 0, 45);
-                 Debug.Log(Vector3.Distance(transform.eulerAngles, to));
-
-                 if (Vector3.Distance(transform.eulerAngles, to) > 0.01f)
-                 {
-                     transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
-                 }
-                 else
-                 {
-                     transform.eulerAngles = to;
-                     rotating = false;
-                 }
-             }*/
         }
-
-
-
-
-        //if (transform.rotation.z >= 0 && transform.rotation.z < 2)
-        //{
-        //    transform.Rotate(Vector3.forward);
-        //}
     }
 
     private void OnBecameInvisible()
